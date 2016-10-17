e0f86aea21f589625b336b7c7fe36f17a296af67
swapnigupta <sg8509@yp.com>
2016-10-16 19:57:03 -0700

    Project 1

---
+++ Plot2.R
@@ -0,0 +1,12 @@
+#Read data from txt file
+dataFile <- "household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+FinalData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+
+#plotting finalData
+datetime <- strptime(paste(FinalData$Date, FinalData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(FinalData$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
---
+++ Plot4.R
@@ -0,0 +1,30 @@
+#Read data from txt file
+dataFile <- "household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+FinalData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+datetime <- strptime(paste(FinalData$Date, FinalData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(FinalData$Global_active_power)
+globalReactivePower <- as.numeric(FinalData$Global_reactive_power)
+voltage <- as.numeric(FinalData$Voltage)
+subMetering1 <- as.numeric(FinalData$Sub_metering_1)
+subMetering2 <- as.numeric(FinalData$Sub_metering_2)
+subMetering3 <- as.numeric(FinalData$Sub_metering_3)
+
+
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2, 2)) 
+
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+
+plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
+
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+
+plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
+
+dev.off()
